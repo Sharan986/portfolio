ccd78a590ba22046b9039f36d0042562c492b199
Sharan <sharansingh9.8.6.0@gmail.com>
2026-07-23 13:34:50 +0530

    feat: implement Splash.tsx component

---
+++ src/components/layout/Splash.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+export default function Splash() {
+  return (
+    <div className="splash" id="splash">
+      <div className="splash-row splash-row-top">
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+      </div>
+      <div className="splash-row splash-row-bottom">
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+        <div className="splash-box"></div>
+      </div>
+    </div>
+  );
+}
